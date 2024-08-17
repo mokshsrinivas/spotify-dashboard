47652297d20725ad30545fe3ee2e19c544752e85
icantthinkofanyotherusername <106741712+icantthinkofanyotherusername@users.noreply.github.com>
2024-08-17 18:37:49 +0800

    dropdown right

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const CLIENT_ID = 'cc085e915c3a444a997efa86c4c2d64c';
-const REDIRECT_URI = 'http://localhost:3000/callback';
+const REDIRECT_URI = 'https://spotify-dashboard-jet.vercel.app/callback';
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}&scope=user-top-read playlist-modify-public playlist-modify-private playlist-read-collaborative playlist-read-private user-library-modify user-library-read user-library-modify`;
 
 const Login = () => (
